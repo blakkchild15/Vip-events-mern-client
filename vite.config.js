53a74f0a2a9fdd84b8989da31da8211703299ec9
blakkchild15 <165008139+blakkchild15@users.noreply.github.com>
2024-08-13 15:13:04 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "https://vip-events-mern-server.onrender.com",
+      "/api": "http://localhost:5000",
     },
   },
 });
